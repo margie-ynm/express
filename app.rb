0a6c3989d31e38af65dbbbb20f5e941da12e2012
Kenneth Black and Margie Nevarez <margie@>
2016-12-07 16:37:23 -0800

    add form and route to create city

--- app.rb
+++ app.rb
@@ -13,3 +13,20 @@ DB = PG.connect({:dbname => 'express'}) # rescue require('./db_init.rb')
 get('/') do
   erb(:index)
 end
+
+# CREATE    POST /cities (in progress)
+# READ ALL  GET  /cities (done)
+# READ ONE  GET /cities/:id
+# UPDATE    PATCH /cities/:id
+# DELETE    DELETE /cities/:id
+
+get('/cities') do
+  @cities = City.all
+  erb(:cities_list)
+end
+
+post('/cities') do
+  name = params.fetch('new-city-name')
+  City.new({:name => name}).save
+  redirect('/cities')
+end
--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,3 +2,23 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+require('spec_helper.rb')
+
+describe("City RESTful routes", {:type => :feature}) do
+  describe("GET Cities") do
+    it('shows a list of all of the cities') do
+      City.new({:name => "Portland, OR"}).save()
+      visit("/cities")
+      expect(page).to have_content("Portland, OR")
+    end
+  end
+  describe("POST Cities") do
+    it('allows a user to create a city') do
+      visit('/cities')
+      fill_in('new-city-name', :with => 'Seattle, WA')
+      click_button('Add City')
+      visit('/cities')
+      expect(page).to have_content('Seattle, WA')
+    end
+  end
+end
